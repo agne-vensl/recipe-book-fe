6b9fe7dcb412cd76e60bc2c86fa7c8db1c4930f4
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-06-29 00:38:06 +0300

    button component

---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button :type="type || 'button'" :class="classes" :disabled="loading">
+    <div>
+      <Loading v-if="loading" small="small" />
+
+      <slot v-else />
+    </div>
+  </button>
+</template>
+
+<script>
+import Loading from './Loading.vue';
+
+export default {
+  name: 'Button',
+  props: ['loading', 'type', 'classes'],
+  components: { Loading },
+};
+</script>
+
+<style scoped>
+button {
+  min-width: 4.8rem;
+}
+
+button div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
