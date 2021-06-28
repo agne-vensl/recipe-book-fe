bbc6b149b4cd5088a3085ce5d5d20b45db936748
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-06-28 16:41:06 +0300

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,82 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-  </div>
+  <Header :loggedIn="loggedIn" />
   <router-view />
 </template>
 
 <script>
+import Header from './components/Header.vue';
+
 export default {
   name: 'App',
+  components: { Header },
+  data() {
+    return {
+      loggedIn: Boolean(localStorage.getItem('token')),
+    };
+  },
+  methods: {
+    updateLoggedIn() {
+      this.loggedIn = Boolean(localStorage.getItem('token'));
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+body {
+  background: rgb(252, 246, 234);
+  color: #222;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+
+.button {
+  border: 1px solid #222;
+  border-radius: 2rem;
+  cursor: pointer;
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  transition: all 0.2s ease;
+}
+
+.button:hover {
+  transform: scale(1.05);
+}
+
+.button:active {
+  background: rgb(165, 165, 165);
+  border-color: rgb(131, 131, 131);
+  color: #fff;
+}
+
+.button.primary {
+  background: rgb(255, 185, 2);
+  border-color: rgb(255, 185, 2);
+  color: #fff;
+}
+
+.button.primary:active {
+  background: transparent;
+  color: #222;
+}
+
+.button.secondary {
+  border-color: rgb(255, 185, 2);
+}
+
+.button.secondary:active {
+  background: rgb(255, 185, 2);
+  color: #fff;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="container">
+    <header>
+      <router-link to="/">
+        <img id="logo" src="../assets/logo.png" alt="top chef logo" />
+      </router-link>
+
+      <nav>
+        <router-link class="link after" to="/">Home</router-link>
+        <router-link class="link after" to="/recipes">Recipes</router-link>
+        <router-link v-if="loggedIn" class="link after" to="/add-recipe">Add Recipe</router-link>
+        <router-link v-if="loggedIn" class="link after" to="/favourites">Favourites</router-link>
+
+        <div id="login-register">
+          <router-link v-if="!loggedIn" class="button secondary" to="/login">Login</router-link>
+          <router-link v-if="!loggedIn" class="button primary" to="/register">Register</router-link>
+          <router-link v-if="loggedIn" @click="logout" class="button primary" to=""
+            >Logout</router-link
+          >
+        </div>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: ['loggedIn'],
+  methods: {
+    logout() {
+      localStorage.removeItem('token');
+      this.$root.updateLoggedIn();
+      this.$router.push('/');
+    },
+  },
+};
+</script>
+
+<style>
+#logo {
+  height: 8rem;
+  margin-top: -1rem;
+}
+
+header {
+  text-align: center;
+  padding: 0.5rem 1rem;
+}
+
+a {
+  color: #222;
+  margin-bottom: 0.5rem;
+  text-decoration: none;
+  text-transform: uppercase;
+}
+
+.link:hover {
+  text-decoration: underline;
+}
+
+@media only screen and (min-width: 768px) {
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.5rem 2rem;
+  }
+
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  #logo {
+    height: 6rem;
+  }
+
+  a {
+    display: inline-block;
+    font-size: 0.9rem;
+    margin-bottom: 0;
+  }
+
+  .link {
+    position: relative;
+    margin-right: 2rem;
+  }
+
+  .link.after::after {
+    background: #222;
+    content: '';
+    display: inline-block;
+    height: 2rem;
+    width: 0.01rem;
+    position: absolute;
+    top: -0.45rem;
+    right: -1rem;
+  }
+
+  #login-register {
+    margin-left: 2rem;
+  }
+
+  #login-register :not(:last-child) {
+    margin-right: 1rem;
+  }
+}
+</style>
