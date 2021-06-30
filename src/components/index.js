7a33513e4dcf1000c58d866217aebcbe5e3a3421
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-06-30 03:24:48 +0300

    added new imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Header } from './Header.vue';
-export { default as Slideshow } from './Slideshow.vue';
 export { default as Button } from './Button.vue';
 export { default as Loading } from './Loading.vue';
 export { default as Notification } from './Notification.vue';
 export { default as Form } from './Form.vue';
+export { default as Content } from './Content.vue';
+export { default as Search } from './Search.vue';
